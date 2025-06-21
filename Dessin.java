8aa98444b486c0ef00687bac4b744eb81dd2812c
Iavo1926 <maxrazaf622@gmail.com>
2025-06-21 16:14:49 +0200

    cpy

--- Dessin.java
+++ Dessin.java
@@ -22,6 +22,7 @@ public class Dessin extends JPanel {
 
     public Dessin(Tsimbazaza tsimbazaza) {
         this.tsimbazaza = tsimbazaza;
+        this.tsimbazaza = tsimbazaza;
         this.totalAnimaux = Animaux.getCount();
         this.carnivoreEatFrames = new HashMap<>();
         staticCarnivoreEatFrames = carnivoreEatFrames;
